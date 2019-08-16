83978fe662287bdf72b373a83b7c896b231bef17
sjwconway <sjwconway@hotmail.com>
2019-08-16 10:20:48 +1000

    sidebar cleanup

--- src/routes.js
+++ src/routes.js
@@ -14,45 +14,14 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import Buttons from "views/Components/Buttons.jsx";
-import Calendar from "views/Calendar/Calendar.jsx";
-import Charts from "views/Charts/Charts.jsx";
+
 import Dashboard from "views/Dashboard/Dashboard.jsx";
-import ErrorPage from "views/Pages/ErrorPage.jsx";
-import ExtendedForms from "views/Forms/ExtendedForms.jsx";
-import ExtendedTables from "views/Tables/ExtendedTables.jsx";
-import FullScreenMap from "views/Maps/FullScreenMap.jsx";
-import GoogleMaps from "views/Maps/GoogleMaps.jsx";
-import GridSystem from "views/Components/GridSystem.jsx";
-import Icons from "views/Components/Icons.jsx";
-import LockScreenPage from "views/Pages/LockScreenPage.jsx";
-import LoginPage from "views/Pages/LoginPage.jsx";
-import Notifications from "views/Components/Notifications.jsx";
-import Panels from "views/Components/Panels.jsx";
-import PricingPage from "views/Pages/PricingPage.jsx";
-import RTLSupport from "views/Pages/RTLSupport.jsx";
-import ReactTables from "views/Tables/ReactTables.jsx";
-import RegisterPage from "views/Pages/RegisterPage.jsx";
-import RegularForms from "views/Forms/RegularForms.jsx";
-import RegularTables from "views/Tables/RegularTables.jsx";
-import SweetAlert from "views/Components/SweetAlert.jsx";
-import TimelinePage from "views/Pages/Timeline.jsx";
-import Typography from "views/Components/Typography.jsx";
-import UserProfile from "views/Pages/UserProfile.jsx";
-import ValidationForms from "views/Forms/ValidationForms.jsx";
-import VectorMap from "views/Maps/VectorMap.jsx";
-import Widgets from "views/Widgets/Widgets.jsx";
-import Wizard from "views/Forms/Wizard.jsx";
+
 
 // @material-ui/icons
-import Apps from "@material-ui/icons/Apps";
 import DashboardIcon from "@material-ui/icons/Dashboard";
-import DateRange from "@material-ui/icons/DateRange";
-import GridOn from "@material-ui/icons/GridOn";
 import Image from "@material-ui/icons/Image";
-import Place from "@material-ui/icons/Place";
-import Timeline from "@material-ui/icons/Timeline";
-import WidgetsIcon from "@material-ui/icons/Widgets";
+
 
 var dashRoutes = [
   {
@@ -73,174 +42,20 @@ var dashRoutes = [
       {
         path: "/dashboard",
         name: "Mercedes Benz",
-        rtlName: "عالتسعير",
-        // mini: "MB",
-        rtlMini: "ع",
-        component: PricingPage,
         layout: "/admin"
       },
       {
         path: "/dashboard",
         name: "Toyota",
-        rtlName: "صودعم رتل",
-        // mini: "RS",
-        rtlMini: "صو",
-        component: RTLSupport,
         layout: "/admin"
       },
       {
         path: "/dashboard",
         name: "Mazda RX",
-        rtlName: "تيالجدول الزمني",
-        // mini: "T",
-        rtlMini: "تي",
-        component: TimelinePage,
         layout: "/admin"
       },
      
     ]
   },
- 
-  {
-    collapse: true,
-    name: "Forms",
-    rtlName: "إستمارات",
-    icon: "content_paste",
-    state: "formsCollapse",
-    views: [
-      {
-        path: "/regular-forms",
-        name: "Regular Forms",
-        rtlName: "أشكال عادية",
-        mini: "RF",
-        rtlMini: "صو",
-        component: RegularForms,
-        layout: "/admin"
-      },
-      {
-        path: "/extended-forms",
-        name: "Extended Forms",
-        rtlName: "نماذج موسعة",
-        mini: "EF",
-        rtlMini: "هوو",
-        component: ExtendedForms,
-        layout: "/admin"
-      },
-      {
-        path: "/validation-forms",
-        name: "Validation Forms",
-        rtlName: "نماذج التحقق من الصحة",
-        mini: "VF",
-        rtlMini: "تو",
-        component: ValidationForms,
-        layout: "/admin"
-      },
-      {
-        path: "/wizard",
-        name: "Wizard",
-        rtlName: "ساحر",
-        mini: "W",
-        rtlMini: "ث",
-        component: Wizard,
-        layout: "/admin"
-      }
-    ]
-  },
-  {
-    collapse: true,
-    name: "Tables",
-    rtlName: "الجداول",
-    icon: GridOn,
-    state: "tablesCollapse",
-    views: [
-      {
-        path: "/regular-tables",
-        name: "Regular Tables",
-        rtlName: "طاولات عادية",
-        mini: "RT",
-        rtlMini: "صر",
-        component: RegularTables,
-        layout: "/admin"
-      },
-      {
-        path: "/extended-tables",
-        name: "Extended Tables",
-        rtlName: "جداول ممتدة",
-        mini: "ET",
-        rtlMini: "هور",
-        component: ExtendedTables,
-        layout: "/admin"
-      },
-      {
-        path: "/react-tables",
-        name: "React Tables",
-        rtlName: "رد فعل الطاولة",
-        mini: "RT",
-        rtlMini: "در",
-        component: ReactTables,
-        layout: "/admin"
-      }
-    ]
-  },
-  {
-    collapse: true,
-    name: "Maps",
-    rtlName: "خرائط",
-    icon: Place,
-    state: "mapsCollapse",
-    views: [
-      {
-        path: "/google-maps",
-        name: "Google Maps",
-        rtlName: "خرائط جوجل",
-        mini: "GM",
-        rtlMini: "زم",
-        component: GoogleMaps,
-        layout: "/admin"
-      },
-      {
-        path: "/full-screen-maps",
-        name: "Full Screen Map",
-        rtlName: "خريطة كاملة الشاشة",
-        mini: "FSM",
-        rtlMini: "ووم",
-        component: FullScreenMap,
-        layout: "/admin"
-      },
-      {
-        path: "/vector-maps",
-        name: "Vector Map",
-        rtlName: "خريطة المتجه",
-        mini: "VM",
-        rtlMini: "تم",
-        component: VectorMap,
-        layout: "/admin"
-      }
-    ]
-  },
-  {
-    path: "/widgets",
-    name: "Widgets",
-    rtlName: "الحاجيات",
-    icon: WidgetsIcon,
-    component: Widgets,
-    layout: "/admin"
-  },
-  {
-    path: "/charts",
-    name: "Charts",
-    rtlName: "الرسوم البيانية",
-    icon: Timeline,
-    component: Charts,
-    layout: "/admin"
-  },
-  {
-    path: "/calendar",
-    name: "Calendar",
-    rtlName: "التقويم",
-    icon: DateRange,
-    component: Calendar,
-    layout: "/admin"
-  }
 ];
 export default dashRoutes;
